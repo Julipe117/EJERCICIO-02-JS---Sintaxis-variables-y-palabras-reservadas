3ce0d07db4717283a6378cd68d0eec10c7058fa1
Julipe117 <110401271+Julipe117@users.noreply.github.com>
2022-08-30 14:46:27 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
-const nombre = "Sergio";
-var edad = 28;
-
-var desarrollador = false;
-
-if(desarrollador){
-    console.log("Si lo soy");
-}
-else{
-    console.log("Todavia no");
-}
-
-const fecha_nacimiento =  new Date(1994,02,04);
-console.log(fecha_nacimiento);
-
-let libro_favorito = {
-    titulo: "La Cupula",
-    autor: "Stephen King",
-    fecha: "Oct 2009",
-    url: "https://www.amazon.es/c%C3%BApula-BEST-SELLER-Stephen-King/dp/8499891098"
-}
-
-console.log(libro_favorito);
+const lista = [
+    "Sergio",
+    28,
+    false,
+    new Date(1994,02,04),
+    {
+        titulo: "La Cupula",
+        autor: "Stephen King",
+        fecha: "Oct 2009",
+        url: "https://www.amazon.es/c%C3%BApula-BEST-SELLER-Stephen-King/dp/8499891098"
+    },
+];
